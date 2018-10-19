b1fe37d023d4be881cb690551db4929ed1c93de4
sebastien-sougnez <sebastien.sougnez@onprvp.fgov.be>
2018-10-19 15:02:21 +0200

    minor changes

--- AreaProg.uTinyMceIcons/Properties/AssemblyInfo.cs
+++ AreaProg.uTinyMceIcons/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.25")]
-[assembly: AssemblyVersion("1.0.0.25")]
-[assembly: AssemblyFileVersion("1.0.0.25")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha-000025")]
+// [assembly: AssemblyVersion("1.0.0.30")]
+[assembly: AssemblyVersion("1.0.0.30")]
+[assembly: AssemblyFileVersion("1.0.0.30")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha-000030")]
